44606af0ef6b10e9153f02459ac3bbf861ba24ab
Jimin0304 <pjws0304@naver.com>
2024-02-13 18:02:13 +0900

    chore: databases 설정 추가

--- pongWorld/pongWorld/settings.py
+++ pongWorld/pongWorld/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pongWorld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pong_world',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
