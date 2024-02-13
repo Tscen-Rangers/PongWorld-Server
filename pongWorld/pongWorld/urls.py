91ecbfdff453877492ef34658ea894920171e186
Jimin0304 <pjws0304@naver.com>
2024-02-13 17:22:59 +0900

    chore: 파일명 변경

--- pongWorld/pongWorld/urls.py
+++ pongWorld/pongWorld/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pongWolrd project.
+URL configuration for pongWorld project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
