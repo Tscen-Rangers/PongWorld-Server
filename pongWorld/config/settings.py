f5bec9eebcfb74b5dd124f41ac98e2ae221125b2
JH713 <strider13@snu.ac.kr>
2024-02-21 17:23:26 +0900

    feat: public chatting

--- pongWorld/config/settings.py
+++ pongWorld/config/settings.py
@@ -194,3 +194,11 @@ SPECTACULAR_SETTINGS = {
 }
 
 ASGI_APPLICATION = 'config.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
