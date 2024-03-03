37f32fd6505c0a28e463b14deb334d3f6566f311
JH713 <strider13@snu.ac.kr>
2024-03-03 21:33:29 +0900

    feat: public chatting

--- pongWorld/websocket/consumers.py
+++ pongWorld/websocket/consumers.py
@@ -4,18 +4,34 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.utils import timezone
 
+from chat.socket.mixins import ChatMixin
 
-class ConnectConsumer(AsyncWebsocketConsumer):
+
+class ConnectConsumer(AsyncWebsocketConsumer, ChatMixin):
     async def connect(self):
-        self.room_group_name1 = 'online_users'
-        self.room_group_name2 = f'player_{self.player_id}'
+        self.user = self.scope['user']
 
+        if self.user.is_authenticated:
+            self.player_id = self.user.id
+            print(self.player_id)
+            self.online_users_group = 'online_users'
+            self.player_group = f'player_{self.player_id}'
 
-        await self.channel_layer.group_add(self.room_group_name1, self.channel_name)
-        await self.channel_layer.group_add(self.room_group_name2, self.channel_name)
+            await self.channel_layer.group_add(self.online_users_group, self.channel_name)
+            await self.channel_layer.group_add(self.player_group, self.channel_name)
 
-        await self.accept()
+            await self.accept()
+        else:
+            await self.close()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name1, self.channel_name)
-        await self.channel_layer.group_discard(self.room_group_name2, self.channel_name)
+        if self.user.is_authenticated:
+            await self.channel_layer.group_discard(self.online_users_group, self.channel_name)
+            await self.channel_layer.group_discard(self.player_group, self.channel_name)
+
+    async def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message_type = text_data_json['type']
+
+        if message_type == 'public_chat':
+            await ChatMixin.handle_public_chat(self, text_data_json)
--- pongWorld/websocket/routing.py
+++ pongWorld/websocket/routing.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from chat.socket import consumers
+from . import consumers
 
-chat_websocket_urlpatterns = [
+websocket_urlpatterns = [
     path('ws/connection/', consumers.ConnectConsumer.as_asgi()),
 ]
