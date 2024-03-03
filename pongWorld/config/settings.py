dd7d2d5c1163d4fe17ec2ef084e7ea575484cb58
JH713 <strider13@snu.ac.kr>
2024-03-03 14:36:24 +0900

    feat: profile image field 수정

--- pongWorld/config/settings.py
+++ pongWorld/config/settings.py
@@ -282,3 +282,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
