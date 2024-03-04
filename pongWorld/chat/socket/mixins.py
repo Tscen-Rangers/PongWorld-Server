cae11a6f9f50dbc4976ddfa97200fa3a37cbfcc5
JH713 <strider13@snu.ac.kr>
2024-03-04 20:41:39 +0900

    feat: private chat

--- pongWorld/chat/socket/mixins.py
+++ pongWorld/chat/socket/mixins.py
@@ -3,10 +3,16 @@ import json
 from channels.db import database_sync_to_async
 from django.utils import timezone
 
+from chat.models import ChatRoom, Message
+from player.models import Player
+
 class ChatMixin:
     async def handle_public_chat(self, text_data_json):
         user = self.user
-        message = text_data_json['message']
+        message = text_data_json.get('message')
+        if message is None:
+            await self.send_error_message("Message not provided")
+            return
 
         await self.channel_layer.group_send(
             self.online_users_group, {
@@ -30,3 +36,113 @@ class ChatMixin:
             "message": message,
             "time": time,
         }, ensure_ascii=False))
+
+    async def handle_private_chat(self, text_data_json):
+        status = text_data_json.get('status')
+        if status is None:
+            await self.send_error_message("Status not provided")
+            return
+
+        if status == 'enter':
+            await self.enter_private_chat(text_data_json)
+        elif status == 'leave':
+            await self.leave_private_chat()
+        elif status == 'message':
+            await self.message_private_chat(text_data_json)
+
+    async def enter_private_chat(self, text_data_json):
+        receiver_id = text_data_json.get('receiver_id')
+        if receiver_id is None:
+            await self.send_error_message("Receiver id not provided")
+            return
+
+        self.chatroom, created = await self.get_or_create_chatroom(receiver_id)
+        if self.chatroom is None:
+            await self.send_error_message("Chatroom creation failed.")
+            return
+        if not created:
+            await self.reset_unread_count()
+
+        self.private_room_group = f'chat_private_{self.chatroom.id}'
+        await self.channel_layer.group_add(self.private_room_group, self.channel_name)
+
+    async def leave_private_chat(self):
+        await self.channel_layer.group_discard(self.private_room_group, self.channel_name)
+        self.chatroom = None
+
+    async def message_private_chat(self, text_data_json):
+        result = await self.new_message(text_data_json)
+        if result is None:
+            await self.send_error_message("Message not provided")
+            return
+
+        await  self.channel_layer.group_send(
+            self.private_room_group, result
+        )
+
+    async def private_message(self, event):
+        user_id = event['user_id']
+        nickname = event['nickname']
+        message = event['message']
+        time = event['time']
+
+        await self.send(text_data=json.dumps({
+                "user_id": user_id,
+                "nickname": nickname,
+                "message": message,
+                "time": time,
+            }, ensure_ascii=False))
+        await self.reset_unread_count()
+
+    @database_sync_to_async
+    def get_or_create_chatroom(self, receiver_id):
+        sorted_ids = sorted([self.user.id, receiver_id])
+
+        try:
+            user1 = Player.objects.get(id=sorted_ids[0])
+            user2 = Player.objects.get(id=sorted_ids[1])
+
+            chatroom, created = ChatRoom.objects.get_or_create(
+                user1=user1, user2=user2,
+                defaults={
+                    'last_sender': user1,
+                    'unread_count': 0,
+                    'last_send_time': timezone.now()
+                }
+            )
+            return chatroom, created
+        except Player.DoesNotExist:
+            return None, False
+        except Exception:
+            return None, False
+
+    @database_sync_to_async
+    def reset_unread_count(self):
+        chatroom = self.chatroom
+        sender = self.user
+        if chatroom.last_sender != sender:
+            chatroom.unread_count = 0
+            chatroom.save()
+
+    @database_sync_to_async
+    def new_message(self, data_json):
+        message = data_json.get('message')
+        if message is None:
+            return None
+
+        user = self.user
+        chatroom = self.chatroom
+        new_message = Message.objects.create(
+            chatroom = chatroom,
+            sender = user,
+            message = message,
+        )
+        formatted_date = new_message.created_at.strftime('%Y-%m-%d %H:%M:%S')
+
+        return {
+            "type": "private.message",
+            "user_id": user.id,
+            "nickname": user.nickname,
+            "message": message,
+            "time": formatted_date
+        }
--- pongWorld/websocket/consumers.py
+++ pongWorld/websocket/consumers.py
@@ -13,7 +13,6 @@ class ConnectConsumer(AsyncWebsocketConsumer, ChatMixin):
 
         if self.user.is_authenticated:
             self.player_id = self.user.id
-            print(self.player_id)
             self.online_users_group = 'online_users'
             self.player_group = f'player_{self.player_id}'
 
@@ -30,8 +29,22 @@ class ConnectConsumer(AsyncWebsocketConsumer, ChatMixin):
             await self.channel_layer.group_discard(self.player_group, self.channel_name)
 
     async def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message_type = text_data_json['type']
+        try:
+            text_data_json = json.loads(text_data)
+        except json.JSONDecodeError:
+            await self.send_error_message("Invalid JSON received")
+            return
+        message_type = text_data_json.get('type')
+        if message_type is None:
+            await self.send_error_message("Message type not provided")
+            return
 
         if message_type == 'public_chat':
             await ChatMixin.handle_public_chat(self, text_data_json)
+        elif message_type == 'private_chat':
+            await ChatMixin.handle_private_chat(self, text_data_json)
+
+    async def send_error_message(self, message):
+        await self.send(text_data=json.dumps({
+            "error": message
+        }, ensure_ascii=False))
