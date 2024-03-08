d496e388459743b8ed8ba9861bd5a9b082ad7c58
Jimin0304 <pjws0304@naver.com>
2024-03-08 20:43:15 +0900

    [#37]feat: 유저 프로필 조회 api 구현

--- pongWorld/player/urls.py
+++ pongWorld/player/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "player"
 
 urlpatterns = [
-    path('online/', views.OnlinePlayerListView.as_view())
+    path('online/', views.OnlinePlayerListView.as_view()),
     path('', views.PlayerRetrieveUpdateDestroyView.as_view(), name="player_detail"),
+    path('profile/<int:user_id>/', views.PlayerProfileView.as_view({'get': 'get_player_profile'}), name="player_profile"),
     path("friends/request/<int:followed_id>/", views.FriendReqResView.as_view({'post': 'request_friend'}), name="request_friend"),
     path("friends/response/<int:friend_id>/", views.FriendReqResView.as_view({'patch': 'response_friend', 'delete': 'response_friend'}), name="response_friend"),
     path("friends/request/send/", views.FriendReqResView.as_view({'get': 'send_req_list'}), name="send_req_list"),
