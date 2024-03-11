78ba0c203410481c098eb6c0992fa4019804ca4c
Jimin0304 <pjws0304@naver.com>
2024-03-11 17:26:31 +0900

    chore: search response에friend_id 추가, 결과 리스트에 자기 자신 제외

--- pongWorld/player/serializers/player.py
+++ pongWorld/player/serializers/player.py
@@ -48,11 +48,12 @@ class SearchPlayerSerializer(serializers.ModelSerializer):
     is_online = serializers.SerializerMethodField()
     friend_status = serializers.SerializerMethodField()
     is_blocking = serializers.SerializerMethodField()
+    friend_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Player
         profile_img = serializers.ImageField(use_url=True)
-        fields = ["id", "nickname", "profile_img", "is_online", "friend_status", "is_blocking"]
+        fields = ["id", "nickname", "profile_img", "is_online", "friend_status", "is_blocking", "friend_id"]
 
     def get_is_online(self, obj) -> bool:
         return obj.online_count > 0
@@ -75,3 +76,11 @@ class SearchPlayerSerializer(serializers.ModelSerializer):
             return True # block
         except Block.DoesNotExist:
             return False  # unblock
+
+    def get_friend_id(self, obj) -> int:
+        me = self.context['request'].user
+        try:
+            friend = Friend.objects.get(Q(follower=me, followed=obj) | Q(follower=obj, followed=me))
+            return friend.id
+        except Friend.DoesNotExist:
+            return 0  # None
