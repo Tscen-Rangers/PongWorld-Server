8ac64c57016c052dcccb33948d72869450a77292
Jimin0304 <pjws0304@naver.com>
2024-03-26 21:43:37 +0900

    chore: 내 프로필 전적 조회도 3개 -> 20개로 변경

--- pongWorld/player/views/player.py
+++ pongWorld/player/views/player.py
@@ -86,7 +86,7 @@ class PlayerProfileView(viewsets.ModelViewSet):
         serializer = PlayerSerializer(current_user, context={'request': request})
         games = (Game.objects.filter(player1=me) | Game.objects.filter(player2=me)) & Game.objects.filter(status=2)
         if games.exists():  # 게임이 하나도 없을 때 예외 처리
-            games = games.order_by('-created_at')[:3]
+            games = games.order_by('-created_at')[:20]
             game_serializer = GameSerializer(games, many=True, context={'request': request, 'user_id': current_user.id})
             games_data = game_serializer.data
         else:
